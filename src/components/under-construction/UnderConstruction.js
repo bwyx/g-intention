ed08dacf2bf236ed3bd08d0ae8b4844ae82c6aab
FOUNDER & CTO l G-INTENTION <info@g-intention.com>
2021-11-18 12:30:01 +0530

    testing pipeline

--- src/components/under-construction/UnderConstruction.js
+++ src/components/under-construction/UnderConstruction.js
@@ -9,7 +9,7 @@ const UnderConstruction = () => {
                 <div className="under-construction-container">
                     <div>
                         <div>
-                            <h1>Coming soon to the world</h1>
+                            <h1>it'll work</h1>
                             <h6>
                             </h6>
                             <button>Explore product</button>
